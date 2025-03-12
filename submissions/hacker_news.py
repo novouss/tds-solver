5fd298235dbeac12ac4b6e48144b3ca6363bc9bd
novouss <raymondgorospe1113@gmail.com>
2025-03-12 22:52:20 +0800

    Updated httpx GET requesting

--- submissions/hacker_news.py
+++ submissions/hacker_news.py
@@ -1,11 +1,17 @@
 
-def hacker_news(url: str, **kwargs) -> str:
-    """ Returns the title of the most recent article on Hacker News from the given URL.
+from typing import Dict, Any
+
+def hacker_news(request: Dict[str: Any]) -> str:
+    """ Returns the link of the most recent article on Hacker News from the given URL.
 
     Args:
-        url (str): The base URL of the Hacker News page.
-        **kwargs: Additional query parameters to append to the URL.
-
+        request (Dict[str: Any]): A dictionary containing the URL and other query parameters. The "url" key is required.
+            Example:
+                {
+                    "url": "https://hnrss.org/newest",
+                    "q": "AWS",  # optional search parameter
+                    "points": "98"  # optional points filter
+                }
     Returns:
         str: The title of the most recent article on Hacker News.
 
@@ -14,30 +20,18 @@ def hacker_news(url: str, **kwargs) -> str:
         ET.ParseError: If there was an error parsing the XML response.
 
     Example:
-    >>> kwargs = {
+    >>> request = {
+    ...    "url": "https://hnrss.org/newest"
     ...    "q": "AWS",
     ...    "points": "98"
     ... }
-    >>> hacker_news("https://hnrss.org/newest", **kwargs)
+    >>> hacker_news(request)
     https://example.org/latest-post
     """
     import httpx
     import xml.etree.ElementTree as ET
-    
-    query = [url]
-
-    if kwargs:
-        query.append("?")
-    
-    for key, value in kwargs.items():
-        query.append(key)
-        query.append("=")
-        query.append(value)
-        query.append("&")
-    
-    query_string = "".join(query)
-
-    response = httpx.get(query_string, verify=False)
+    response = httpx.get(request.pop("url"), params=request, verify=False)
+    response.raise_for_status()
     root = ET.fromstring(response.content)
     items = root.find(".//item")
     latest = items.find("link").text
