89cfb1ca96f7a55876ddb0733befa2c331cb95c3
Raymond Brian Dominguiano Gorospe <Raymondbrian.Gorospe@straive.com>
2025-03-18 23:25:06 +0800

    Created generate_embeddings and remade output of llm_embeddings

--- authentication.py
+++ authentication.py
@@ -1,5 +1,6 @@
 
 import os
+from typing import List
 from openai import OpenAI
 
 URL = "https://llmfoundry.straive.com/openai/v1/"
@@ -9,3 +10,32 @@ client = OpenAI(
     base_url=URL,
     api_key=KEY
 )
+
+model = "gpt-4o-mini"
+
+def ask_something(prompt: str) -> str:
+    response = client.chat.completions.create(
+        model = model,
+        messages = [
+            { "role": "user", "content": prompt }
+        ]
+    )
+    return response.choices[0].message.content
+
+def ask_someone(system: str, prompt: str) -> str:
+    response = client.chat.completions.create(
+        model = model,
+        messages = [
+            { "role": "system", "content": system },
+            { "role": "user", "content": prompt }
+        ]
+    )
+    return response.choices[0].message.content
+
+def generate_embeddings(text: str) -> List[float]:
+    response = client.embeddings.create(
+        model = "text-embedding-3-small",
+        input = text
+    )
+    embeddings = response.data[0].embedding
+    return embeddings
--- submissions/llm_embeddings.py
+++ submissions/llm_embeddings.py
@@ -1,26 +1,12 @@
 
 from typing import List
 
-def llm_embeddings(text: str) -> List[float]:
-    """ Generates embeddings for a given text.
-
-    Args:
-        text (str): The text to generate embeddings for.
-
-    Returns:
-        List[float]: A list of floats representing the embeddings.
-
-    Raises:
-        Exception: If an error occurs during the API call.
-
-    Example:
-        >>> llm_embeddings("hello world")
-        [0.234, 0.567, 0.890]
-    """
-    from authentication import client
-    response = client.embeddings.create(
-        model = "text-embedding-3-small",
-        input = text
-    )
-    embeddings = response.data[0].embedding
-    return embeddings
+def llm_embeddings(text: List[str]) -> str:
+    import json
+    
+    body = {
+        "model": "text-embedding-3-small",
+        "input": text
+    }
+    
+    return json.dumps(body)
