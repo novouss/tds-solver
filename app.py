e41e3178fe721522340e1c42e1a7d07afc8cfeea
novouss <raymondgorospe1113@gmail.com>
2025-03-25 21:14:14 +0800

    Refactor run function to accept optional file parameter

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ from fastapi import FastAPI, UploadFile, status
 from fastapi.responses import JSONResponse
 from fastapi import Form, File
 
+import os
 import chromadb
+from typing import Optional
 
-from helpers.authentication import client
-from submissions import tasks
+import helpers.authentication as auth
 
 app = FastAPI()
 
@@ -24,10 +25,10 @@ FUNCTIONS_DB = chromadb.PersistentClient(path="chromadb")
 FUNC_COLLECTION = FUNCTIONS_DB.get_or_create_collection(name="functions")
 
 @app.post("/api/", status_code=status.HTTP_200_OK)
-async def run(question: str = Form(...), file: UploadFile = File(...)):
+async def run(question: str = Form(...), file: Optional[UploadFile] = File(None)):
     """ Answer any graded assignment question and returns the result/answer for said question.
     
-    Args:
+    Args:`
         question (str): The question of the user.
         file (file): The file the user is requesting to be downloaded/used.
         
@@ -36,29 +37,32 @@ async def run(question: str = Form(...), file: UploadFile = File(...)):
     """
 
     # Creates the filepath for both extraction and retrieval
-    filepath = "/".join([DATA_PATH, file.filename])
-    # Creates the directory
-    with open(filepath, "wb") as f:
-        f.write(await file.read())
-    
-    # response = client.chat.completions.create(
-    #     model = "gpt-4o-mini",
-    #     messages = [{
-    #         "role": "system",
-    #         "content": "Reply only with the answer the user is looking for no added texts"
-    #     },
-    #     {
-    #         "role": "user",
-    #         "content": question
-    #     }]
-    # )
-    
+    if file:
+        filepath = os.path.join(DATA_PATH, file.filename)
+        # Creates the directory
+        with open(filepath, "wb") as f:
+            f.write(await file.read())
+
+    answering = FUNC_COLLECTION.query(
+        query_embeddings = auth.generate_embeddings(question),
+        n_results = 1
+    )
+
+    docstring = answering["metadatas"][0][0]["docstring"]
+    name = answering["documents"][0][0]
+    function_call = auth.create_function_call(name, docstring)
+
+    # response = auth.ask_tools(question, function_call)
     # answer = respones.choices[0].message.content
     
     answer = {
         "question": question,
-        "file_name": file.filename,
-        "file_location": filepath,
+        "name": name,
+        "function_call": function_call,
+        # "response": response
+        # "answer": answering,
+        # "file_name": file.filename,
+        # "file_location": filepath,
     }
     
     return JSONResponse(content={ "answer": answer })
