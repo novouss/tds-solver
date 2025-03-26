badf26a091f97d48138fd50ae1322f702abc4e39
Raymond Brian Dominguiano Gorospe <Raymondbrian.Gorospe@straive.com>
2025-03-27 00:05:10 +0800

    Added docstring

--- submissions/host_portfolio.py
+++ submissions/host_portfolio.py
@@ -1,6 +1,21 @@
 
 def host_portfolio(html_tag: str) -> str:
-    
+    """ Inserts a given HTML tag into a specific location in the index.html file and returns the URL of the hosted portfolio.
+
+    Args: 
+        html_tag (str): The HTML tag to be inserted into the index.html file.
+
+    Returns: 
+        str: The URL of the hosted portfolio.
+
+    Raises: 
+        FileNotFoundError: If the index.html file does not exist. IOError: If there is an error writing to the index.html file.
+
+    Example: 
+        >>> host_portfolio("New Content") 
+        'https://novouss.github.io/tds-solver/'
+    """
+        
     static_index = "./index.html"
     
     with open(static_index, "r") as file:
@@ -14,4 +29,4 @@ def host_portfolio(html_tag: str) -> str:
         file.writelines(lines)
     
     return "https://novouss.github.io/tds-solver/"
-        
+    
